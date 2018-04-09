4a1005fb8b95933610ef597e7658870a77c52790
marietrull <marietrull@gmail.com>
2018-04-09 11:43:17 -0500

    Commit 2: Tip Calculator express application created.

--- express_lab.js
+++ express_lab.js
@@ -9,6 +9,18 @@ app.get('/greeting/:name', (req, res) => {
   res.send('Hello ' + greeting[req.params.name])
 });
 
+
+//TIP CALCULATOR
+
+app.get('/tipCalc/:total/:tip', (req, res) => {
+	const total = req.params.total;
+	const tipPerc = req.params.tip;
+	const tipAmount = total/tipPerc;
+	//const tip = req.params.tip;
+ 	res.send('Tip = ' + tipAmount);
+});
+
+
 app.listen(3000, () => {
 	console.log('Server is listening on Port 3000');
 	
