4e10e0f19be2b2476c5b1ca60e173fe04fa9ec52
marietrull <marietrull@gmail.com>
2018-04-09 11:56:58 -0500

    Commit 3: Magic 8 Ball express application created.

--- express_lab.js
+++ express_lab.js
@@ -20,6 +20,18 @@ app.get('/tipCalc/:total/:tip', (req, res) => {
  	res.send('Tip = ' + tipAmount);
 });
 
+//MAGIC EIGHT BALL
+
+const magicAnswers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+
+app.get('/magic/:question', (req,res) => {
+	const magicQuest = req.params.question;
+	const randNum = Math.floor(Math.random()*20);
+	const randAnswer = magicAnswers[randNum]; 
+	res.send('Question: ' + magicQuest + 'Answer: ' + randAnswer);
+	
+})
+
 
 app.listen(3000, () => {
 	console.log('Server is listening on Port 3000');
